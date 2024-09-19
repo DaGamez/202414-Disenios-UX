458d5034834a2be34c8ea0c8363f6ed866bc2e82
mar-alas <m.alas@uniandes.edu.co>
2024-09-18 22:28:23 -0700

    Agregando login pag1 (In Progress)

--- Web/web-app/src/app/app.module.ts
+++ Web/web-app/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WebAppHomeComponent } from './web-app-home/web-app-home.component';
 import { WebAppHomeModule } from './web-app-home/web-app-home.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
