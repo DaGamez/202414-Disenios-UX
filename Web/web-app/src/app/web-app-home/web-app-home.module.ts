be2dc50d07e568ba49810e5756cb9685c7f30df4
DaGamez <da.gamez96@uniandes.edu.co>
2024-09-20 21:29:22 -0500

    ajuste ruta de home a configuracion

--- Web/web-app/src/app/web-app-home/web-app-home.module.ts
+++ Web/web-app/src/app/web-app-home/web-app-home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WebAppHomeComponent } from './web-app-home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [WebAppHomeComponent],
   exports: [WebAppHomeComponent]
