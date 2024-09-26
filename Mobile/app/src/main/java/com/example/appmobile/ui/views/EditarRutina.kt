83616d2a46913383b71f90fc48c82a00168b7530
DaGamez <da.gamez96@uniandes.edu.co>
2024-09-25 20:51:36 -0500

    se completa pantalla editar rutina

--- Mobile/app/src/main/java/com/example/appmobile/ui/views/EditarRutina.kt
+++ Mobile/app/src/main/java/com/example/appmobile/ui/views/EditarRutina.kt
@@ -3,8 +3,10 @@ package com.example.appmobile.ui.views
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
@@ -14,8 +16,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.appmobile.ui.ActivityCard
 import com.example.appmobile.ui.BackgroundImage
 import com.example.appmobile.ui.ButtonAppPrincipal
+import com.example.appmobile.ui.ButtonAppSecondary
+import com.example.appmobile.ui.InputTextApp
+import com.example.appmobile.ui.IntermediateHeader
 import com.example.appmobile.ui.ScreenHeader
 
 @Composable
@@ -33,11 +39,22 @@ fun EditarRutinaScreen(navHostController: NavHostController){
                 verticalArrangement = Arrangement.SpaceBetween,
                 horizontalAlignment = Alignment.CenterHorizontally
             ){
-                ScreenHeader(text="Editar Rutina")
-                Spacer(modifier = Modifier.height(200.dp))
+                ScreenHeader(text="Edita tu Rutina")
+                InputTextApp(value="Colegio Niños",label="Nombre de la Rutina")
+                IntermediateHeader(text="Actividades de tu Rutina")
+                ActivityCard(nombre_rutina="Bañarse",description="Inicio y Duracion",navHostController)
+                ActivityCard(nombre_rutina="Desayunar",description="Inicio y Duracion",navHostController)
                 ButtonAppPrincipal(text="Añadir Actividad",route="nuevaActividad",navHostController)
-                ButtonAppPrincipal(text="Cancelar",route="listadoRutinas",navHostController)
-                ButtonAppPrincipal(text="Guardar",route="listadoRutinas",navHostController)
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement= Arrangement.SpaceBetween,
+                    verticalAlignment=Alignment.CenterVertically
+
+                ){
+                    ButtonAppSecondary(text="Cancelar",route="listadoRutinas",navHostController)
+                    ButtonAppSecondary(text="Guardar",route="listadoRutinas",navHostController)
+                }
             }
         }
 
